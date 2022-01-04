f97e12745640ab68c745cbcf56659230e80e1654
fabiandroguettdimabe <fabian.droguett@dimabe.cl>
2022-01-04 15:04:28 -0300

    add block of lot in different process

--- dimabe_manufacturing/models/unpelled_dried.py
+++ dimabe_manufacturing/models/unpelled_dried.py
@@ -457,5 +457,10 @@ class UnpelledDried(models.Model):
         }
 
     def print_all_out_serial(self):
+        serials = self.out_lot_id.stock_production_lot_serial_ids.filtered(lambda x: x.printed)
+        serials.write({
+            'printed': True,
+            'to_print': False
+        })
         return self.env.ref('dimabe_manufacturing.action_print_all_out_serial') \
-            .report_action(self.out_lot_id)
+            .report_action(serials)
